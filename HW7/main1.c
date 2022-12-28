881cc54f215cc9f08025a74b65431787b05ced6e
b1129025 <120005392+b1129025@users.noreply.github.com>
2022-12-28 21:15:15 +0800

    Update main1.c

--- HW7/main1.c
+++ HW7/main1.c
@@ -3,7 +3,7 @@
 int main(void) {
     int n = 7;
     for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= n; j++) {
+        for (int j = 0; j <= n; j++) {
             if (j <= n - i) {
                 printf(" ");
             } else {
