5cbf8bc8039ccf0f909787a9b40c5f6500bea917
b1129025 <120005392+b1129025@users.noreply.github.com>
2022-12-28 16:35:12 +0800

    Update main0.c

--- HW6/main0.c
+++ HW6/main0.c
@@ -2,16 +2,14 @@
 
 int main()
 {
-    int i = 10;  // 預設 i 為 10
-
-    // 判斷 i 是否為 2 的冪次方
+    int i = 10;  
     if ((i & (i - 1)) == 0)
     {
-        printf("%d 是 2 的冪次方。\n", i);
+        printf("%d is true\n", i);
     }
     else
     {
-        printf("%d 不是 2 的冪次方。\n", i);
+        printf("%d is false\n", i);
     }
 
     return 0;
