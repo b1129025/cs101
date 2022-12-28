f56a341355d93a8e91e98c432ed94c8fef24b2e9
b1129025 <120005392+b1129025@users.noreply.github.com>
2022-12-28 10:56:41 +0800

    Create main1.c

---
+++ HW6/main1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+  int i = 10;
+
+  if (i % 15 == 0) {
+    printf("Love IU\n");
+  } else if (i % 3 == 0) {
+    printf("Love\n");
+  } else if (i % 5 == 0) {
+    printf("IU\n");
+  } else {
+    printf("%d\n", i);
+  }
+
+  return 0;
+}
